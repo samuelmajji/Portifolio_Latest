aeb2072b94b546abe2b9dcf9a078d1e5af966e71
samuelmajji <samuelmajji0@gmail.com>
2025-07-28 23:00:33 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./pages/Hero";
 import Projects from "./pages/Projects";
 import MouseGlow from "./components/MouseGlow";
-import Dairy from "./pages/Dairy";
+// import Dairy from "./pages/Dairy";
 import JournalApp from "./pages/NewJournal";
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/project" element={<Projects />} />
-          <Route path="/Dairy" element={<Dairy />} />
-          {/* <Route path="/Journal" element={<JournalApp />} /> */}
+          {/* <Route path="/Dairy" element={<Dairy />} /> */}
+          <Route path="/Journal" element={<JournalApp />} />
           {/* <Route path="/J" element={<JournalApp />} /> */}
         </Routes>
       </div>
